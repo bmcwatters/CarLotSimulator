ec49b1895331151064e4398275c9330c91d6c846
bryan mcwatters <bmcwatters@comcast.net>
2021-12-09 12:57:40 -0500

    initial commit

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -12,17 +12,52 @@ namespace CarLotSimulator
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
-
+            // DONE!
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
-
+            //DONE!
             //*************BONUS*************//
-
+            //DONE!
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            CarLot lot = new CarLot();
+            
 
-            //*************BONUS X 2*************//
+
+            Car bmw = new Car();
+            bmw.Year = 2016;
+            bmw.Make = "BMW";
+            bmw.Model = "328i";
+            bmw.EngineNoise = "Vroom";
+            bmw.HonkNoise = "Beep boop";
+            bmw.IsDrivable = true;
+
+            bmw.MakeEngineNoise();
+            bmw.MakeHonkNoise();
+
+            lot.CarList.Add(bmw);
+
+            Car hyundai = new Car() { Year = 2020, Make = "Hyundai", Model = "Sonata", EngineNoise = "Help", HonkNoise = "Beep beep" };
+
+            hyundai.MakeEngineNoise();
+            hyundai.MakeHonkNoise();
+
+            lot.CarList.Add(hyundai);
+
+            Car ford = new Car(2019, "Ford", "F150", "Vroom vroom", "Look out");
+
+            ford.MakeEngineNoise();
+            ford.MakeHonkNoise();
+
+            lot.CarList.Add(ford);
+
+            //*************BONUS X 2*************//Done!
+            foreach (var car in lot.CarList)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+            }
+
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
